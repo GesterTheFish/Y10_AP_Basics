3f0c6561512aa1c9689da26504ae3e16362d0beb
GesterTheFish <Coddyterei@gmail.com>
2025-08-08 10:28:26 +1200

    Variable Basics

---
+++ P_02_Variables.py
@@ -0,0 +1,24 @@
+# Code to learn about variables...
+
+# Variables can be a string (note the speech marks)
+greeting = "Hello World"
+
+# Variables can be a number
+fav_num = 7
+num_string = "5"
+
+# Constants are uppercase
+PI = 3.14
+# Don't start variables with numbers
+a_01_my_num = 7
+
+# Variables can be boolean (True / False)
+love_programming = True
+
+# This variable has a problem!
+my_name = "Miss G"
+
+print (fav_num * 2)
+print (num_string * 5)
+
+
